bb5cc1f877b610738b2aa5f652fe02159deba306
Jaime Reis <jaime_reis18@outlook.pt>
2020-08-24 15:50:25 +0000

    Database created

---
+++ GandaSpents/GandaSpents/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GandaSpents.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Spent> Spents { get; set; }
+        public DbSet<SpentEntity> SpentEntities { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ GandaSpents/GandaSpents/Models/Spent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GandaSpents.Models
+{
+    public class Spent
+    {
+        public int Id { get; set; }
+        public int SpentEntityId { get; set; }
+        public SpentEntity SpentEntity { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
